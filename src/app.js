57592b6f6caf6dcfc59e5df9b7bc7365baeb83e2
Yibeltal <yibeltalawoke058@gmail.com>
2024-06-11 13:01:07 +0300

    4th

--- src/app.js
+++ src/app.js
@@ -167,7 +167,7 @@ app.use((err, req, res, next) => {
 });
 
 
-mongoose.connect('mongodb://localhost:27017/ERPDB', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
